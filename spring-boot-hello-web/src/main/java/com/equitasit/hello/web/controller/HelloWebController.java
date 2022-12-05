a71618ef590af73364a9fa5b134cf2fea1d75ede
srini-equitasit <srini@equitasitinc.com>
2022-12-05 23:18:13 +0530

    spring boot web project

--- spring-boot-hello-web/src/main/java/com/equitasit/hello/web/controller/HelloWebController.java
+++ spring-boot-hello-web/src/main/java/com/equitasit/hello/web/controller/HelloWebController.java
@@ -1,17 +1,32 @@
 package com.equitasit.hello.web.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.equitasit.hello.web.model.EmpDto;
 
 @Controller
 public class HelloWebController {
 
-	@GetMapping("/hello-web")
-	public String greeting(@RequestParam(name = "name", required = false, defaultValue = "") String name, Model model) {
-		model.addAttribute("name", name);
-		return "greeting";
+	private List<EmpDto> empList = new ArrayList<>();
+
+	@GetMapping("/emp_details")
+	public String greetingForm(Model model) {
+		model.addAttribute("emp", new EmpDto());
+		return "emp_reg";
+	}
+
+	@PostMapping("/emp_details")
+	public String greetingSubmit(@ModelAttribute EmpDto emp, Model model) {
+		empList.add(emp);
+		model.addAttribute("empList", empList);
+		return "result";
 	}
 
 }
---
+++ spring-boot-hello-web/src/main/java/com/equitasit/hello/web/model/EmpDto.java
@@ -0,0 +1,25 @@
+package com.equitasit.hello.web.model;
+
+public class EmpDto {
+
+	private String name;
+
+	private String dept;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+}
